81ee8dfe402324e3f5308a61c88c1eaa36fe0c61
Datian <zdatian@gmail.com>
2018-12-28 18:38:18 -0500

    added player

---
+++ Python/Player.py
@@ -0,0 +1,4 @@
+class Player:
+    def __init__(self, card1, card2):
+        self.card1 = card1
+        self.card2 = card2
--- Python/Table.py
+++ Python/Table.py
@@ -1,5 +1,5 @@
 import random
-from .Player import Player
+import Player
 class Table:
     cards = ["2D","2C","2H","2S",
     "3D","3C","3H","3S",
@@ -14,12 +14,14 @@ class Table:
     "QD","QC","QH","QS",
     "KD","KC","KH","KS",
     "1D", "1C", "1H", "1S"]
-
+    players = []
     def __init__(self, numPlayers):
         self.numPlayers = numPlayers
+        self.dealPlayers()
     def dealPlayers(self):
         cards = self.cards
         numPlayers = self.numPlayers
+        players = self.players
         print()
         for num in range(1, numPlayers + 1 ):
             print("Player " + str(num))
@@ -27,10 +29,13 @@ class Table:
             cards.remove(card1)
             card2 = random.choice(cards)
             cards.remove(card2)
-
+            players.append(Player.Player(card1, card2))
             print(card1)
             print(card2)
             print()
+
+
+
     def dealBoard(self, progression):
         cards = self.cards
         if(progression == "flop"):
@@ -50,7 +55,7 @@ class Table:
             cards.remove(river)
             print(river)
 newTable = Table(5)
-newTable.dealPlayers()
-newTable.dealBoard("flop")
-newTable.dealBoard("turn")
-newTable.dealBoard("river")
+#newTable.dealPlayers()
+#newTable.dealBoard("flop")
+#newTable.dealBoard("turn")
+#newTable.dealBoard("river")
