dd23b61254608ad83fe68f6f0f0b4f248907ad73
haiyaoliu <haiyaoliu@umass.edu>
2019-01-05 20:56:49 -0500

    basic sort

--- java/HandEvaluator.java
+++ java/HandEvaluator.java
@@ -3,68 +3,56 @@ import java.util.*;
 
 public class HandEvaluator {
     private static final int[] results = {7,8,4,5,0,1,2,3,6};
-    private static final String[] names = {"quads","straight flush","straight","flush","high card","pair","two pair", "placeholder", "trips","full house"};
-    public static final int[] evaluator = {140, 160, 80, 100, 0, 20, 40,-100, 60, 120 };
+    private static final String[] names = {"quads","straight flush","straight","flush","high card","pair","two pair", "gote", "trips","full house"};
 
     private HandEvaluator() {}
 
-
     public static final int evaluate(Card[] cards){
-        long id = getId(cards);
-        //System.out.println(id);
-        int handRating = evaluator[(int)id];
-        handRating += rankOfTopFrequency(cards) + 2;
-        //System.out.println(handRating);
-
-        return handRating;
-    }
-
-
-    public static final long getId(Card[] cards) {
-        /*
-
-            step 1: sort by rules {frequency, rank}
-            step 2: categorize first 5 cards
-                switch:
-                    7 cards:    if type is Q,FH - return type
-                                else continue
-                    6 cards:    if type is Q,FH,T,TP - return type
-                                else continue
-                    5 cards:    return type
-            step 3: categorize other combinations
-                switch:
-                    7 cards:    possibilities - trips,twopair,pair,highcard
-                                alt check for straight: ?
-                                alt check for flush: counting suits?
-                    6 cards:    possibilities - pair,highcard
-
-
-        */
-
-        // hand has hole cards, a number, a top combination, and id
-        // in table you can do handeval.top
-
-        //step 1, %15 categorize
         long id = 0;
+        int max = 0;
+        int next = 0;
+        int[] sorted = new int[5];
 
         int[] counts = new int[13];
         for(Card c : cards) {
-            if((++counts[c.rank]) > 0) {
+            if(++counts[c.rank] > 0) {
                 id |= (1L<<(c.rank*4))*((1L<<counts[c.rank])-1);
+                if(max < counts[c.rank]) {
+                    max = counts[c.rank];
+                }
             }
+        }
+
+        // 41 32 311 221 2111 11111
+        int p = 0;
+        for(int t = 0; t < 2; t++) {
+            for(int c = 12; c > -1; c--) {
+                if(counts[c] == max) {
+                    int j;
+                    for(j = 0; j < max; j++) {
+                        sorted[p+j] = c;
+                    }
+                    p += j;
+                } else {
+                    if( counts[c] > 0 ) next = counts[c];
+                }
+            }
+            max = next;
         }
 
         id = id % 15 - 1;
 
-        //step 2, check for straight and/or flush
         if(id == 4) {
             if(straight(cards)) { id-=2; }
             if(flush(cards)) { id-=1; }
         }
-        return id;
-        //System.out.println(" " + id + " " + names[(int)id] + " " + evaluator[(int)id]);
-        //printHand(id);
-        //return evaluator[(int)id] + topRank(cards);
+
+        for(int i : sorted) {
+            System.out.print(Card.ranks[i] + " ");
+        }
+        System.out.println();
+
+        return (int)id;
     }
 
     public static int rankOfTopFrequency(Card[] cards){
@@ -118,8 +106,8 @@ public class HandEvaluator {
         Card[] topFive = new Card[5];
         int topNum = -1;
         for(int i=0;i<7;i++){
-            for(int j=i;j<7;j++){
-                if(i == j) {continue;}
+            for(int j=i+1;j<7;j++){
+                //if(i == j) {continue;} redundant
                 // System.out.println("first excluded is " + i);
                 // System.out.println("second excluded is " + j);
 
--- java/Test.java
+++ java/Test.java
@@ -1,49 +1,43 @@
 public class Test {
     public static void main(String[] args) {
-        Card[] cards = new Card[7];
+        Card[] cards = new Card[5];
 
         //straight flush
-        for(int i = 0; i < 7; i++) {
+        for(int i = 0; i < 5; i++) {
             cards[i] = new Card(i,0);
         }
-        //HandEvaluator.sevenCardEvaluate(cards);
+        HandEvaluator.evaluate(cards);
 
         //full house
         for(int i = 0; i < 5; i++) {
             cards[i] = new Card(i%2,(i+1)%4);
         }
-        //HandEvaluator.evaluate(cards);
+        HandEvaluator.evaluate(cards);
 
         //flush
         int suit = (int)(Math.random()*4);
         for(int i = 0; i < 5; i++) {
             cards[i] = new Card(i*2,suit);
         }
-        //HandEvaluator.evaluate(cards);
+        HandEvaluator.evaluate(cards);
 
         //straight
         for(int i = 0; i < 5; i++) {
             cards[i] = new Card(i,(int)(Math.random()*4));
         }
-        //HandEvaluator.evaluate(cards);
+        HandEvaluator.evaluate(cards);
 
         //trips
         for(int i = 0; i < 5; i++) {
             cards[i] = new Card((i/3)*(int)(Math.random()*10),i%4);
         }
-        //HandEvaluator.evaluate(cards);
+        HandEvaluator.evaluate(cards);
 
         //two pair
         for(int i = 0; i < 5; i++) {
             cards[i] = new Card(i/2,i%4);
         }
-        //HandEvaluator.evaluate(cards);
-
-        Card[] sevenCards = new Card[7];
-        for(int i = 0; i < 7; i++) {
-            sevenCards[i] = new Card();
-        }
+        HandEvaluator.evaluate(cards);
         //HandEvaluator.sevenCardEvaluate(sevenCards);
-        HandEvaluator.compareHands(cards, sevenCards);
     }
 }
